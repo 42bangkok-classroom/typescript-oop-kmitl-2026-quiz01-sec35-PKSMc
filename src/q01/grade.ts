dc0c2dc151bfb305eedfb9c4335dd4558fba91af
Sirawit Pattanawaw <68011145@kmitl.ac.th>
2025-12-15 09:44:33 +0700

    first try

---
+++ src/q01/grade.ts
@@ -0,0 +1,22 @@
+const input = Number(process.argv[2]);
+if (Number.isNaN(input) || input > 100 || input < 0){
+	console.log("Invalid Input");
+	process.exit();
+}
+
+if (input >= 80){
+	console.log("A");
+}
+else if (input >= 70){
+	console.log("B");
+}
+else if (input >= 60){
+	console.log("C");
+}
+else if (input >= 50){
+	console.log("D");
+}
+else if (input <= 50){
+	console.log("F");
+}
+
